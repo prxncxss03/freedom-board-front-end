2ac40340ad2d5351cc963f251d2185d3ff53a487
prxncxss03 <pocon.evangeline@gmail.com>
2022-06-26 12:23:53 +0800

    fix widths and paddings in mobile

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,8 +5,8 @@ export const Navigation = ({cmshow}) => {
         <div className='flex justify-center flex-col items-center'>
       
             <h1 className="font-indie text-[2.5rem] w-fit  sm:mt-3 mt-4  font-bold text-white ">FREEDOM BOARD</h1>
-            <p className='text-xl text-white sm:pt-2 pt-0'>Keep it Short and Simple! 💋</p>
-            <button onClick={cmshow} className='sm:text-2xl text-3xl flex text-white fixed sm:decoration-wavy  sm:decoration-from-font sm:underline-offset-2 font-bold sm:underline sm:hover:decoration-solid px-4 py-2  bg-green-600 right-0 bottom-0 md:top-0 h-fit rounded-lg sm:m-2 my-6 mx-4 shadow-md z-20 
+            <p className='text-xl text-white  pt-0'>Keep it Short and Simple! 💋</p>
+            <button onClick={cmshow} className='sm:text-2xl text-3xl flex text-white fixed sm:decoration-wavy  sm:decoration-from-font sm:underline-offset-2 font-bold sm:underline sm:hover:decoration-solid px-4 py-2  bg-green-600 right-0 bottom-0 md:top-0 h-fit rounded-lg md:m-2 my-6 mx-4 shadow-md z-20 
             
             '><FaStickyNote className='mr-2 sm:inline-flex hidden '></FaStickyNote> <span className="sm:hidden ">+</span><p className="hidden sm:inline-flex">Post a message</p></button>
       
